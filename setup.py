e1125d596bcab30e1b6c90fb4c83ebabaad3fdfa
Florian Golemo <fgolemo@gmail.com>
2020-10-22 12:02:55 -0400

    bugfix

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@ from setuptools import setup
 setup(
     name="stanford_quad",
     version="1.0",
-    install_requires=["tqdm", "numpy", "gym"],
+    install_requires=["tqdm", "numpy", "gym", "transforms3d"],
     extras_require={
         "sim": ["matplotlib", "pybullet", "gym", "opencv-python", "torchvision"],
-        "robot": [
-            "transforms3d",
-            "UDPComms @ git+https://github.com/stanfordroboticsclub/UDPComms@master#egg=UDPComms",
-        ],
+        "robot": ["UDPComms @ git+https://github.com/stanfordroboticsclub/UDPComms@master#egg=UDPComms",],
     },
 )
