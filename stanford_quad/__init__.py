089484a6e47a2a58bc76d590dac5b389d467729e
Florian Golemo <fgolemo@gmail.com>
2020-10-24 15:41:54 -0400

    added lower frequency variant

--- stanford_quad/__init__.py
+++ stanford_quad/__init__.py
@@ -27,176 +27,38 @@ ACTION_TYPE = ["Relative", "Absolute", "Incremental"]
 RANDOM_ROT = [0, 1, 10, 100]
 ACTION_SCALING = [1.0, 2.0, 4.0] + list(np.arange(0.05, 0.56, 0.1))
 GAIT_FACTOR = [0.0, 0.2, 0.4, 0.6, 0.8, 0.95]
+CTRL_FREQ = [60, 10]
+SECONDS_OF_SIM = 2
 
 for headlessness in HEADLESSNESS:
-    for action_type in ACTION_TYPE:
-        # for action_smoothing in ACTION_SMOOTHING:
-        for action_scaling in ACTION_SCALING:
-            for random_rot in RANDOM_ROT:
-                for gait_fact in GAIT_FACTOR:
-                    name = (
-                        f"Pupper-Walk-{action_type}-"
-                        f"aScale_{action_scaling:.2}-"
-                        # f"aSmooth_{action_smoothing}-"
-                        f"gFact_{gait_fact}-"
-                        f"RandomZRot_{random_rot}-{headlessness}-v0"
-                    )
-                    # print(name)
-                    register(
-                        id=name,
-                        entry_point="stanford_quad.envs:WalkingEnv",
-                        kwargs={
-                            "debug": (False if headlessness == "Headless" else True),
-                            "steps": 120,
-                            "relative_action": True if action_type == "Relative" else False,
-                            "incremental_action": True if action_type == "Incremental" else False,
-                            "action_scaling": action_scaling,
-                            "action_smoothing": 1,
-                            "random_rot": (0, 0, random_rot),
-                            "gait_factor": gait_fact,
-                        },
-                        max_episode_steps=120,
-                    )
-
-
-###### LEGACY  - WILL REMOVE SOON
-
-
-for headlessness in HEADLESSNESS:
-    register(
-        id=f"Pupper-Walk-Absolute-{headlessness}-v0",
-        entry_point="stanford_quad.envs:WalkingEnv",
-        kwargs={
-            "debug": (False if headlessness == "Headless" else True),
-            "steps": 120,
-            "relative_action": False,
-            "action_scaling": 1,
-        },
-        max_episode_steps=120,
-    )
-    register(
-        id=f"Pupper-Walk-Relative-{headlessness}-v0",
-        entry_point="stanford_quad.envs:WalkingEnv",
-        kwargs={
-            "debug": (False if headlessness == "Headless" else True),
-            "steps": 120,
-            "relative_action": True,
-            "action_scaling": 1,
-        },
-        max_episode_steps=120,
-    )
-    for scale_down in list(np.arange(0.05, 0.5, 0.05)):
-        register(
-            id=f"Pupper-Walk-Relative-ScaledDown_{scale_down:.2}-{headlessness}-v0",
-            entry_point="stanford_quad.envs:WalkingEnv",
-            kwargs={
-                "debug": (False if headlessness == "Headless" else True),
-                "steps": 120,
-                "relative_action": True,
-                "action_scaling": scale_down,
-            },
-            max_episode_steps=120,
-        )
-        # print(f"Pupper-Walk-Relative-ScaledDown_{scale_down:.2}-{headlessness}-v0")
-    register(
-        id=f"Pupper-Walk-Relative-ScaledDown-RandomZRot-{headlessness}-v0",
-        entry_point="stanford_quad.envs:WalkingEnv",
-        kwargs={
-            "debug": (False if headlessness == "Headless" else True),
-            "steps": 120,
-            "relative_action": True,
-            "action_scaling": 0.3,
-            "random_rot": (0, 0, 15),
-        },
-        max_episode_steps=120,
-    )
-    register(
-        id=f"Pupper-Walk-Relative-ScaledNSmoothed3-RandomZRot-{headlessness}-v0",
-        entry_point="stanford_quad.envs:WalkingEnv",
-        kwargs={
-            "debug": (False if headlessness == "Headless" else True),
-            "steps": 120,
-            "relative_action": True,
-            "action_scaling": 0.3,
-            "action_smoothing": 3,
-            "random_rot": (0, 0, 15),
-        },
-        max_episode_steps=120,
-    )
-    register(
-        id=f"Pupper-Walk-Relative-ScaledNSmoothed5-RandomZRot-{headlessness}-v0",
-        entry_point="stanford_quad.envs:WalkingEnv",
-        kwargs={
-            "debug": (False if headlessness == "Headless" else True),
-            "steps": 120,
-            "relative_action": True,
-            "action_scaling": 0.3,
-            "action_smoothing": 5,
-            "random_rot": (0, 0, 15),
-        },
-        max_episode_steps=120,
-    )
-    register(
-        id=f"Pupper-Walk-Relative-Smoothed5-RandomZRot-{headlessness}-v0",
-        entry_point="stanford_quad.envs:WalkingEnv",
-        kwargs={
-            "debug": (False if headlessness == "Headless" else True),
-            "steps": 120,
-            "relative_action": True,
-            "action_scaling": 1,
-            "action_smoothing": 5,
-            "random_rot": (0, 0, 15),
-        },
-        max_episode_steps=120,
-    )
-    register(
-        id=f"Pupper-Walk-Relative-RewardStable0.5-{headlessness}-v0",
-        entry_point="stanford_quad.envs:WalkingEnv",
-        kwargs={
-            "debug": (False if headlessness == "Headless" else True),
-            "steps": 120,
-            "relative_action": True,
-            "action_scaling": 1,
-            "reward_coefficients": (0.1, 1, 0.5),
-        },
-        max_episode_steps=120,
-    )
-    register(
-        id=f"Pupper-Walk-Relative-RewardStable0.5-ScaledDown3-{headlessness}-v0",
-        entry_point="stanford_quad.envs:WalkingEnv",
-        kwargs={
-            "debug": (False if headlessness == "Headless" else True),
-            "steps": 120,
-            "relative_action": True,
-            "action_scaling": 0.3,
-            "reward_coefficients": (0.1, 1, 0.5),
-        },
-        max_episode_steps=120,
-    )
-    register(
-        id=f"Pupper-Walk-Relative-RewardStable0.5-ScaledDown-RandomZRot-{headlessness}-v0",
-        entry_point="stanford_quad.envs:WalkingEnv",
-        kwargs={
-            "debug": (False if headlessness == "Headless" else True),
-            "steps": 120,
-            "relative_action": True,
-            "action_scaling": 0.3,
-            "random_rot": (0, 0, 15),
-            "reward_coefficients": (0.1, 1, 0.5),
-        },
-        max_episode_steps=120,
-    )
-    register(
-        id=f"Pupper-Walk-Relative-RewardStable0.5-ScaledNSmoothed-RandomZRot-{headlessness}-v0",
-        entry_point="stanford_quad.envs:WalkingEnv",
-        kwargs={
-            "debug": (False if headlessness == "Headless" else True),
-            "steps": 120,
-            "relative_action": True,
-            "action_scaling": 0.3,
-            "action_smoothing": 3,
-            "random_rot": (0, 0, 15),
-            "reward_coefficients": (0.1, 1, 0.5),
-        },
-        max_episode_steps=120,
-    )
+    for ctrl_freq in CTRL_FREQ:
+        for action_type in ACTION_TYPE:
+            # for action_smoothing in ACTION_SMOOTHING:
+            for action_scaling in ACTION_SCALING:
+                for random_rot in RANDOM_ROT:
+                    for gait_fact in GAIT_FACTOR:
+                        name = (
+                            f"Pupper-Walk-{action_type}-"
+                            f"aScale_{action_scaling:.2}-"
+                            # f"aSmooth_{action_smoothing}-"
+                            f"freq_{ctrl_freq}-"
+                            f"gFact_{gait_fact}-"
+                            f"RandomZRot_{random_rot}-{headlessness}-v0"
+                        )
+                        print(name)
+                        register(
+                            id=name,
+                            entry_point="stanford_quad.envs:WalkingEnv",
+                            kwargs={
+                                "debug": (False if headlessness == "Headless" else True),
+                                "steps": SECONDS_OF_SIM * ctrl_freq,
+                                "relative_action": True if action_type == "Relative" else False,
+                                "incremental_action": True if action_type == "Incremental" else False,
+                                "action_scaling": action_scaling,
+                                "action_smoothing": 1,
+                                "random_rot": (0, 0, random_rot),
+                                "gait_factor": gait_fact,
+                                "control_freq": ctrl_freq,
+                            },
+                            max_episode_steps=SECONDS_OF_SIM * ctrl_freq,
+                        )
