04380fc45628db20a292fb45e59ac019fda91267
Florian Golemo <fgolemo@gmail.com>
2021-02-24 22:07:11 -0500

    added arm ik and colored ergo model

--- scripts/19-test-ik-for-arm.py
+++ scripts/19-test-ik-for-arm.py
@@ -12,33 +12,20 @@ import numpy as np
 
 class ArmIK:
     def __init__(self):
-        self.chain = Chain(name='pupper_ergo', links=[
-            OriginLink(),
-            URDFLink(
-                name="twistybase",
-                translation_vector=[0, 0, .03],
-                orientation=[0, 0, 0],
-                rotation=[0, 0, 1],
-            ),
-            URDFLink(
-                name="shoulder",
-                translation_vector=[0, 0, .0285],
-                orientation=[0, 0, 0],
-                rotation=[1, 0, 0],
-            ),
-            URDFLink(
-                name="elbow",
-                translation_vector=[0, 0, .085],
-                orientation=[0, 0, 0],
-                rotation=[1, 0, 0],
-            ),
-            URDFLink(
-                name="tip",
-                translation_vector=[0, .155, 0],
-                orientation=[1.57, 0, 0],
-                rotation=[1, 0, 0],
-            )
-        ])
+        self.chain = Chain(
+            name="pupper_ergo",
+            links=[
+                OriginLink(),
+                URDFLink(
+                    name="twistybase", translation_vector=[0, 0, 0.03], orientation=[0, 0, 0], rotation=[0, 0, 1],
+                ),
+                URDFLink(
+                    name="shoulder", translation_vector=[0, 0, 0.0285], orientation=[0, 0, 0], rotation=[1, 0, 0],
+                ),
+                URDFLink(name="elbow", translation_vector=[0, 0, 0.085], orientation=[0, 0, 0], rotation=[1, 0, 0],),
+                URDFLink(name="tip", translation_vector=[0, 0.155, 0], orientation=[1.57, 0, 0], rotation=[1, 0, 0],),
+            ],
+        )
 
     @staticmethod
     def _ik2pupperarm(joints):
@@ -55,26 +42,16 @@ class ArmIK:
 
 armik = ArmIK()
 
-# target_position = np.array([.05, .12, .14])
-target_position = np.array([-.1, .17, .18])
-
-iterations = 1000
-start = time.time()
-for _ in range(iterations):
-    joints = armik.pos2joints(target_position)
-diff = time.time() - start
-print (f"that took {diff/iterations}s on avg, so {1/(diff/iterations)}Hz")
-
-print (joints)
+target_position = np.array([-0.10, 0.15, 0.10])
+joints = armik.pos2joints(target_position)
+print(joints)
 armik._makeMoveCmd(joints)
 
 import matplotlib.pyplot as plt
 
 fig, ax = plot_utils.init_3d_figure()
-my_chain.plot(my_chain.inverse_kinematics(target_position), ax, target=target_position)
-ax.set_xlim(-.2, .2)
-ax.set_ylim(-.2, .2)
-# plt.ylim(0, 150)
-# plt.zlim(0, 150)
-ax.set_zlim(-0,.4)
+armik.chain.plot(armik.chain.inverse_kinematics(target_position), ax, target=target_position)
+ax.set_xlim(-0.2, 0.2)
+ax.set_ylim(-0.2, 0.2)
+ax.set_zlim(-0, 0.4)
 plt.show()
--- scripts/20-test-urdf-for-arm.py
+++ scripts/20-test-urdf-for-arm.py
@@ -7,11 +7,7 @@ import numpy as np
 # Create the bullet physics engine environment
 physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
-# p.resetDebugVisualizerCamera(
-#     cameraDistance=0.45,
-#     cameraYaw=135,
-#     cameraPitch=-45,
-#     cameraTargetPosition=[0, 0, 0])
+p.resetDebugVisualizerCamera(cameraDistance=0.45, cameraYaw=135, cameraPitch=-45, cameraTargetPosition=[0, 0, 0])
 p.setGravity(0, 0, -10)  # good enough
 frequency = 240  # Hz
 p.setTimeStep(1 / frequency)
@@ -22,8 +18,7 @@ p.loadURDF("plane.urdf")
 
 # Robot model starting position
 startPos = [0, 0, 0]  # xyz
-startOrientation = p.getQuaternionFromEuler(
-    [0, 0, 0])  # rotated around which axis? # np.deg2rad(90)
+startOrientation = p.getQuaternionFromEuler([0, 0, 0])  # rotated around which axis? # np.deg2rad(90)
 
 # Actually load the URDF file into simulation, make the base of the robot unmoving
 robot = p.loadURDF("./ergourdf.urdf.xml", startPos, startOrientation, useFixedBase=1)
@@ -31,7 +26,7 @@ robot = p.loadURDF("./ergourdf.urdf.xml", startPos, startOrientation, useFixedBa
 for i in range(p.getNumJoints(robot)):
     print(p.getJointInfo(robot, i))
 
-motors = [0,1,2]
+motors = [0, 1, 2]
 
 # Container for debug inputs
 debugParams = []
@@ -50,12 +45,11 @@ for i in range(frequency * 30):
     for i in range(len(motors)):
         pos = (math.pi / 2) * p.readUserDebugParameter(debugParams[i])
         motorPos.append(pos)
-        p.setJointMotorControl2(
-            robot, motors[i], p.POSITION_CONTROL, targetPosition=pos)
+        p.setJointMotorControl2(robot, motors[i], p.POSITION_CONTROL, targetPosition=pos)
 
     p.stepSimulation()
 
-    time.sleep(1. / frequency)
+    time.sleep(1.0 / frequency)
 
 print(time.time() - start)
 
